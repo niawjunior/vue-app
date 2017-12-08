273eb83719405d743f9e45f4737fe925bf1a6cea
niawjunior <pasupol@dome.cloud>
2017-12-08 11:47:37 +0700

    add hero section

--- vue-app/src/components/Home.vue
+++ vue-app/src/components/Home.vue
@@ -1,16 +1,53 @@
 <template>
   <div class="home">
-    <h1>HOME</h1>
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">{{ heading }}</h1>
+          <div class="is-two-thirds column is-paddingless">
+            <h2 class="subtitle is-4">{{ subheading }}</h2>
+          </div>
+          <a class="button is-large is-primary" id="learn">Learn more</a>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'home'
+  name: 'home',
+  data() {
+    return {
+      heading: 'The Answer Company',
+      subheading: 'The intelligence, technology and human experise you need to find trusted answer.'
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="sass" scoped>
 @import '../mq'
+.hero 
+  background: url('../assets/company.jpg')
+  background-size:cover
+
+  .title
+    color:#fff !important
+    +mobile
+      font-weight: bold
+    +tablet
+      font-size: 2.5rem
+    +desktop
+      font-size: 4rem
+      margin-top: 2rem
+      font-weight: bold
+  .subtitle
+    color:#fff !important
+
+  .button
+    margin-top: 2rem
+h2 
+  margin: 1.5rem 0.2rem 0 !important
 </style>
